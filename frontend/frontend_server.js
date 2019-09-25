10db41430370aa027c74a287d13ab9c057644a07
ganino <secretlyrobert@gmail.com>
2019-09-25 16:24:34 -0700

    add frontend

---
+++ frontend/frontend_server.js
@@ -0,0 +1,52 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+let dbConn = require('./authenticator.js')
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.set('view engine', 'ejs'); // set up ejs for templating #todo do we want this
+
+/**
+ * FIRSt TIME sETUP: add admin user
+ * she can be deleted and recreated with a new password, this is just the default
+ * also TODO dont hard code this
+ */
+
+const ADMIN_USERNAME = 'admin';
+const ADMIN_PASSWORD = 'a';
+
+if (!dbConn.userExists(ADMIN_USERNAME)) {
+    dbConn.createNewUser(ADMIN_USERNAME, ADMIN_PASSWORD)
+}
+
+// required for passport
+app.use(session({
+    secret: 'secret', // session secret //TODO CHANGE
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('Listening on ' + port);
