3e84a2b5aacf8ae082ceb122f4ddc26f4fac6424
ganino <secretlyrobert@gmail.com>
2019-10-01 10:58:41 -0700

    fix req flashing + grade getting

--- scrape.js
+++ scrape.js
@@ -14,32 +14,26 @@ module.exports = {
             args: [email, password]
         };
 
-        new Promise(function(resolve, reject) {
+        return new Promise(function(resolve, reject) {
 
             PythonShell.run('./scrape.py', options, (err, results) => {
 
-                console.log("results");
-                console.log(results);
+                // console.log("results");
+                // console.log(results);
+
+                let resp = results[0];
 
                 if (err) {
                     // console.error("ERROR:"  + err);
                     resolve({success: false, message: "Error getting grades."});
-                    return
+                } else if (resp.success === true) {
+                    resolve({success: true, new_grades: resp.grades})
+                } else {
+                    return resolve({success: false, message: resp.message})
                 }
-
-                if (results.success === true) {
-                    resolve({success: true, new_grades: results.grades})
-
-                }
-
-                console.log("aaaaa");
-                return resolve({success: false, message: results.message})
-
                 // console.log(results);
             })
 
-        }).then(r => {
-            return r;
         });
 
     },
