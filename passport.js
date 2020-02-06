402f5cb98376ec32f893ed408548dd1b09e8dc68
jjoelj <hijoelj@gmail.com>
2020-02-05 22:53:58 -0800

    Removed lodash, finalized sync grades on login. Currently causes longer sign in time.

--- passport.js
+++ passport.js
@@ -1,7 +1,7 @@
 // load all the things we need
-const LocalStrategy = require('passport-local').Strategy;
-const authent = require('./authenticator.js');
-const bcrypt = require('bcryptjs');
+const LocalStrategy = require("passport-local").Strategy;
+const authent = require("./authenticator.js");
+const bcrypt = require("bcryptjs");
 module.exports = function (passport) {
 
     // =========================================================================
@@ -18,17 +18,20 @@ module.exports = function (passport) {
     // used to deserialize the user
     passport.deserializeUser(function (username, done) {
         usr = authent.getUser(username);
-        return done(null, usr)
+        return done(null, usr);
     });
 
     // =========================================================================
     // LOCAL LOGIN =============================================================
     // =========================================================================
-    passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, ]
-        usernameField: 'username', passwordField: 'password', passReqToCallback: true
-        // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-    }, function (req, username, password, done) {
+    passport.use("local-login", new LocalStrategy({
+                                                      // by default, local strategy uses username and password, ]
+                                                      usernameField: "username",
+                                                      passwordField: "password",
+                                                      passReqToCallback: true
+                                                      // allows us to pass in the req from our route (lets us check if
+                                                      // a user is logged in or not)
+                                                  }, function (req, username, password, done) {
         if (username) {
             username = username.toLowerCase();
         }
@@ -39,14 +42,19 @@ module.exports = function (passport) {
             let user = authent.getUser(username);
             // if no user is found, return the message
             if (!user) {
-                return done(null, false, req.flash('loginMessage', 'User Not Found.'));
+                return done(null, false, req.flash("loginMessage", "User Not Found."));
             }
 
-            bcrypt.compare(password, user.password, function (err, res) {
+            bcrypt.compare(password, user.password, async function (err, res) {
                 if (res) {
+                    if (user.schoolPassword) {
+                        let resp = authent.decryptAndGet(user.username, password);
+                        let schoolPass = resp.message;
+                        await authent.updateGrades(user.username, schoolPass);
+                    }
                     return done(null, user);
                 } else {
-                    return done(null, false, req.flash('loginMessage', 'Incorrect Password.'));
+                    return done(null, false, req.flash("loginMessage", "Incorrect Password."));
                 }
             });
 
