75d9485f4485a903bf0f5726034508088d5b8fe2
jjoelj <hijoelj@gmail.com>
2020-02-27 11:49:39 -0800

    Bug fixes

--- authenticator.js
+++ authenticator.js
@@ -297,7 +297,7 @@ module.exports = {
                 this.addNewWeightDict(lc_username, i, grade_update_status.new_grades[i].class_name);
             }
         }
-        for (let i = grade_update_status.new_grades.length - 1; i < userRef.value().appearance.classColors.length; i++) {
+        for (let i = grade_update_status.new_grades.length; i < userRef.value().appearance.classColors.length; i++) {
             userRef.value().appearance.classColors.pop();
         }
         userRef.assign({grades: grade_update_status.new_grades}).write();
