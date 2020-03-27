255c9c8fd99fb6b879e5ad7c54bf8731942fafa8
jjoelj <hijoelj@gmail.com>
2020-03-26 22:18:36 -0700

    Debugging

--- routes.js
+++ routes.js
@@ -139,6 +139,7 @@ module.exports = function (app, passport) {
 
     app.get("/checkUpdateBackground", [isLoggedIn], (req, res) => {
         let resp = authenticator.checkUpdateBackground(req.user.username);
+        console.log(req.user.username, resp);
         res.status(200).send(resp.message);
     });
 
