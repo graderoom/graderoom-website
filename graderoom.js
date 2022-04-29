4c62d11b437f8ae22cec4e735cea0cffed216f2c
Joel Jothiprakasam <hijoelj@gmail.com>
2022-04-29 17:27:38 -0500

    Allow posting larger form bodies for whatever reason.

--- graderoom.js
+++ graderoom.js
@@ -51,8 +51,8 @@ mongo.config(mongoUrl, productionEnv, isBetaServer).then(() => {
 
         app.use(morgan("dev")); // log every request to the console
         app.use(cookieParser()); // read cookies (needed for auth)
-        app.use(bodyParser.json()); // get information from html forms
-        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json({limit: '1mb'})); // get information from html forms
+        app.use(bodyParser.urlencoded({limit: '1mb', extended: true}));
 
         app.set("view engine", "ejs"); // set up ejs for templating #todo do we want this
 
