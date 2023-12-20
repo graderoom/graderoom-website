6688d2b5f94a7fbb33f9ee4ab6c657eaa07d5d98
Joel Jothiprakasam <hijoelj@gmail.com>
2023-12-19 20:33:44 -0800

    Fix typo that caused error. Fix sending pp and tc for no reason
    (cherry picked from commit e11729a124fb5e4d20dd62696352376f57ca2425)

--- server/dbClient.js
+++ server/dbClient.js
@@ -2414,7 +2414,7 @@ const _updateNotification = async (db, username, id, update) => {
     if (!res.success) {
         return res;
     }
-    let trimmed = res.alerts.notifications;
+    let trimmed = res.data.value.alerts.notifications;
     let index = trimmed.findIndex(i => i.id === id);
     if (index === -1) {
         return {success: false, data: {message: "Invalid ID", log: `Invalid updateNotification id=${id}`}};
