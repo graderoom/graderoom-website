6bf336c840f2f4526f0e2944ba39c87eed9de0d8
Joel Jothiprakasam <hijoelj@gmail.com>
2026-05-07 18:26:34 -0500

    Make backup work on a cronjob

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 DATE=$(date +%Y-%m-%d-%H-%M-%S-%3N)
 
 # Set the backup directory name
-BACKUP_DIR="../mongodumps"
+BACKUP_DIR="$HOME/mongodumps"
 
 # Create the backup directory if it doesn't exist
 mkdir -p $BACKUP_DIR
@@ -14,4 +14,7 @@ mongodump --out $BACKUP_DIR/"$DATE" --gzip
 
 # Upload to Google Drive
 PARENT_ID=$(cat ~/.gdrive/parent_id)
-gdrive upload --parent "$PARENT_ID" $BACKUP_DIR/"$DATE" --recursive
+/usr/local/bin/gdrive files upload --parent "$PARENT_ID" --recursive $BACKUP_DIR/"$DATE"
+
+# Delete backups older than 7 days
+find $BACKUP_DIR -maxdepth 1 -type d -mtime +7 -exec rm -rf {} +
